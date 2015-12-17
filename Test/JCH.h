9da1be9d4c65a651258dc349f768020ae32332ca
wt <1246338572@qq.com>
2015-12-17 16:41:30 +0800

    zuixin

---
+++ Test/JCH.h
@@ -0,0 +1,13 @@
+//
+//  JCH.h
+//  Test
+//
+//  Created by qianfeng on 15/12/17.
+//  Copyright (c) 2015年 wt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JCH : RootViewController
+
+@end
---
+++ Test/JCH.m
@@ -0,0 +1,13 @@
+//
+//  JCH.m
+//  Test
+//
+//  Created by qianfeng on 15/12/17.
+//  Copyright (c) 2015年 wt. All rights reserved.
+//
+
+#import "JCH.h"
+
+@implementation JCH
+
+@end
