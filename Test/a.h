73b082a9c51d632d6c5d4a74b46f27301857f6c5
wt <1246338572@qq.com>
2015-12-17 17:42:42 +0800

    1w

---
+++ Test/a.h
@@ -0,0 +1,13 @@
+//
+//  a.h
+//  Test
+//
+//  Created by qianfeng on 15/12/17.
+//  Copyright (c) 2015年 wt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface a : RootViewController
+
+@end
---
+++ Test/a.m
@@ -0,0 +1,13 @@
+//
+//  a.m
+//  Test
+//
+//  Created by qianfeng on 15/12/17.
+//  Copyright (c) 2015年 wt. All rights reserved.
+//
+
+#import "a.h"
+
+@implementation a
+
+@end
